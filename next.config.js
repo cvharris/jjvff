3855e519f94d053740592f7f72949eda836ce2ba
Charlie Harris <charris1014@gmail.com>
2023-01-30 00:07:32 -0600

    Adds gh-pages workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
